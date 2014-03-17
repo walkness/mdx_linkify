f767107653851dd145b2046731e4b0142135f934
Raitis Stengrevics <dagrevis@gmail.com>
2014-03-17 21:09:55 +0200

    Add fix for imports

--- mdx_linkify/__init__.py
+++ mdx_linkify/__init__.py
@@ -1,6 +1,7 @@
+from __future__ import absolute_import
 import sys
 
-from mdx_linkify import makeExtension
+from mdx_linkify.mdx_linkify import makeExtension
 
 
 assert makeExtension  # Silences pep8.
